a8f5e1ffaa6603bf702570019778523c0d99817a
TainanKyle <kylehuang121@gmail.com>
2024-11-05 02:48:31 +0800

    add step4 with linear blending

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import cv2
 import numpy as np
-from utils import find_homography, warp_and_stitch
+from utils import find_homography
 from step1_2 import detect_and_describe, match_features
+from step4 import warp
 
 def stitch_images(img1, img2):
     # 1. Interest points detection & feature description by SIFT
@@ -15,13 +16,14 @@ def stitch_images(img1, img2):
     H = find_homography(keypoints1, keypoints2, matches)
     
     # 4. Warp image to create panoramic image
-    result = warp_and_stitch(img1, img2, H)
+    result = warp(img1, img2, H)
     
     return result
 
-img1 = cv2.imread('data\TV1.jpg')
-img2 = cv2.imread('data\TV2.jpg')
+img1 = cv2.imread('data/S1.jpg')
+img2 = cv2.imread('data/S2.jpg')
 
 result = stitch_images(img1, img2)
+cv2.imwrite('test.png', result)
 
 cv2.destroyAllWindows()
---
+++ step4.py
@@ -0,0 +1,40 @@
+import numpy as np
+import cv2
+
+def warp(img1, img2, H):
+    h1, w1 = img1.shape[:2]
+    h2, w2 = img2.shape[:2]
+    stitch_img = np.zeros((h1, w1 + w2, 3), dtype='uint8')
+    max_w = 0
+
+    for i in range(stitch_img.shape[0]):
+        for j in range(stitch_img.shape[1]):
+            cur_point = np.array([j, i, 1]).reshape(3, 1)
+            transform_point = H @ cur_point
+            transform_point /= transform_point[2]
+
+            transform_x = int(transform_point[0, 0])
+            transform_y = int(transform_point[1, 0])
+
+            # with img2 parts
+            if 0 <= transform_x < w2 and 0 <= transform_y < h2:
+                max_w = max(max_w, j)
+                stitch_img[i, j] = img2[transform_y, transform_x]
+
+    stitch_img = stitch_img[:, :max_w]
+
+    non_zero = np.argmax(stitch_img != 0, axis=1)
+    non_zero = non_zero[non_zero > 0]
+    overlap_l = np.min(non_zero)    # the left column of the overlap parts
+
+    # linear blending
+    for i in range(h1):
+        for j in range(w1):
+            alpha = (j - overlap_l) / (w1 - overlap_l)
+            alpha = np.clip(alpha, 0, 1)
+            stitch_img[i, j] = alpha * stitch_img[i, j] + (1 - alpha) * img1[i, j]
+
+    return stitch_img
+
+    # cv2.imshow('Image', stitch_img)
+    # cv2.waitKey(0)
